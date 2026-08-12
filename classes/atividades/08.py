5e994473e748765352b22f0e0f3373b21f39c4a8
annewilcke-gif <anne_wilcke@estudante.sesisenai.org.br>
2026-08-12 17:53:17 +0000

    ok

---
+++ classes/atividades/08.py
@@ -0,0 +1,43 @@
+class PetVirtual:
+    def __init__(self, nome: str):
+        self.nome = nome
+        self.fome = 5
+        self.felicidade = 5
+
+    def alimentar(self):
+        if self.fome > 0:
+            self.fome -= 2
+            
+            if self.fome < 0:
+                self.fome = 0
+            print(f"{self.nome} foi alimentado! Fome atual: {self.fome}")
+        else:
+            print(f"{self.nome} já está de barriga cheia!")
+
+    def brincar(self):
+        self.felicidade += 2
+        self.fome += 1
+        print(f"Você brincou com {self.nome}! Felicidade: {self.felicidade} | Fome: {self.fome}")
+
+    def status(self):
+        print(f"\n--- Status de {self.nome} ---")
+        print(f"Fome: {self.fome}")
+        print(f"Felicidade: {self.felicidade}")
+        
+        if self.fome >= 8:
+            print(f"Atenção: {self.nome} precisa comer!")
+        print("-------------------------\n")
+
+
+meu_pet = PetVirtual("Pou")
+
+meu_pet.status()
+
+meu_pet.brincar()
+meu_pet.brincar()
+
+meu_pet.alimentar()
+meu_pet.alimentar()
+meu_pet.alimentar()
+
+meu_pet.status()
